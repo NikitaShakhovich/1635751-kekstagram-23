39c0806822ba82c4c97f8c35fdf53bab75540bd4
NikitaShakhovich <7149382@tut.by>
2021-07-04 12:31:59 +0300

    Добавил модули, разделил имеющийся код на модули.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,23 @@
 // модуль, который создаёт данные.
+import {RANDOM_DESCRIPTIONS, RANDOM_NAMES, RANDOM_MESSAGES, GENERATED_QUANTITY_OBJECTS, AvatarName, RandomLike} from './constants.js';
+import {randomInteger, getRandomArrayElement} from './util.js';
+const getUniquePhotoInfo = function (item, index) {
+  const RANDOM_NUMBER = index + 1;
+  return {
+    id: RANDOM_NUMBER,
+    url: `photos/${RANDOM_NUMBER}.jpg`,
+    description: getRandomArrayElement (RANDOM_DESCRIPTIONS),
+    likes: randomInteger (RandomLike.MIN, RandomLike.MAX),
+    comments: [
+      {
+        id: _.shuffle(_.range(1,1001))[0],
+        avatar: `img/avatar-${randomInteger (AvatarName.MIN, AvatarName.MAX)}.svg`,
+        message: getRandomArrayElement (RANDOM_MESSAGES),
+        name: getRandomArrayElement (RANDOM_NAMES),
+      },
+    ],
+  };
+};
+
+// eslint-disable-next-line no-unused-vars
+const photoInfosNew = new Array(GENERATED_QUANTITY_OBJECTS).fill('').map(getUniquePhotoInfo);
--- js/util.js
+++ js/util.js
@@ -1,1 +1,17 @@
 // модуль с вспомогательными функциями;
+
+// случайное положительное число
+// https://learn.javascript.ru/task/random-int-min-max
+const randomInteger = (min, max) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+// случайный элемент из массива
+const getRandomArrayElement = (element) => element[_.random(0, element.length -1)];
+
+// проверка длины комментария
+// eslint-disable-next-line no-unused-vars
+const checkCommentLength = (comment, maxLenght) => (comment.lenght <= maxLenght);
+
+export {randomInteger, getRandomArrayElement};
