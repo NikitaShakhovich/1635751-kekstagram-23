2f901343073ab5e782956fa34e6655f1c5ed8bbc
NikitaShakhovich <7149382@tut.by>
2021-07-13 16:19:55 +0300

    Исправлены замечания. Отступы, имена констант. Заведен модуль для миниатюр. Создал DOM элементы и заполнил их данными

--- js/data.js
+++ js/data.js
@@ -21,4 +21,6 @@ const getUniquePhotoInfo = function (item, index) {
 };
 
 // eslint-disable-next-line no-unused-vars
-const photosInfosNew = Array.from({ length: PHOTO_AMOUNT }, (value, index) => index).map(getUniquePhotoInfo);
+const getPhotosInfosNew = () => Array.from({ length: PHOTO_AMOUNT }, (value, index) => index).map(getUniquePhotoInfo);
+
+export {getPhotosInfosNew};
